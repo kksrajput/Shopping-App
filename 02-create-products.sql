f5ba2f8822075ac32e4c30f6498a14a079a08f55
Kaushal Kant Singh <55576102+kksrajput@users.noreply.github.com>
2022-11-06 17:30:12 +0530

    Sha schema definitions

---
+++ 02-create-products.sql
@@ -0,0 +1,71 @@
+-- -----------------------------------------------------
+-- Schema full-stack-shopping-app
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `full-stack-shopping`;
+
+-- DROP SCHEMA IF EXISTS `full-stack-shopping`;
+CREATE SCHEMA `full-stack-shopping`;
+
+
+USE `full-stack-shopping` ;
+
+
+CREATE TABLE IF NOT EXISTS `full-stack-shopping`.`product_category` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `category_name` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE=InnoDB
+AUTO_INCREMENT = 1;
+
+
+CREATE TABLE IF NOT EXISTS `full-stack-shopping`.`product` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `sku` VARCHAR(255) DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
+  `unit_price` DECIMAL(13,2) DEFAULT NULL,
+  `image_url` VARCHAR(255) DEFAULT NULL,
+  `active` BIT DEFAULT 1,
+  `units_in_stock` INT(11) DEFAULT NULL,
+   `date_created` DATETIME(6) DEFAULT NULL,
+  `last_updated` DATETIME(6) DEFAULT NULL,
+  `category_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_category` (`category_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`)
+) 
+ENGINE=InnoDB
+AUTO_INCREMENT = 1;
+
+
+INSERT INTO product_category(category_name) VALUES ('BOOKS');
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('TechBooks-101', 'Spring in Action', 'Learn Spring',
+'assets/images/products/books.png'
+,1,100,19.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('TechBooks-102', 'Spring Framework Tutorial', 'Learn Spring',
+'assets/images/products/books.png'
+,1,100,29.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('TechBooks-103', 'MicroServices', 'Learn Microservices',
+'assets/images/products/books.png'
+,1,100,24.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('TechBooks-104', '189 Coding interview', 'Learn Coding',
+'assets/images/products/books.png'
+,1,100,29.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('TechBooks-105', 'Java: A to Z', 'Learn Java',
+'assets/images/products/books.png'
+,1,100,24.99,1, NOW());
