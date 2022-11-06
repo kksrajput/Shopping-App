9be6f17bea33287ea922b2c592a9c264ba23625b
kksrajput <kaushalkantsingh@gmail.com>
2022-11-06 19:31:27 +0530

    Sha strt

---
+++ spring-boot-shoppingApp/src/main/java/com/app/springbootshoppingApp/SpringBootShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.springbootshoppingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShoppingAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-shoppingApp/src/test/java/com/app/springbootshoppingApp/SpringBootShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootshoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
