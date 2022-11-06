879ae6550a3711376aa47086f0d087c405232dd8
kksrajput <kaushalkantsingh@gmail.com>
2022-11-06 23:25:19 +0530

    Sha disable rest

---
+++ spring-boot-shoppingApp/src/main/java/com/app/springbootshoppingApp/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.app.springbootshoppingApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.app.springbootshoppingApp.entity.Product;
+import com.app.springbootshoppingApp.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,CorsRegistry cors) {
+		HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+		
+		// disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    
+	}
+}
