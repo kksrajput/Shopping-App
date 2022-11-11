78acfcbcbb989ace9e50b0378ae86dfabf346a2a
kksrajput <kaushalkantsingh@gmail.com>
2022-11-11 18:20:22 +0530

    Sha backend fixes+ filterById

--- spring-boot-shoppingApp/src/main/java/com/app/springbootshoppingApp/dao/ProductCategoryRepository.java
+++ spring-boot-shoppingApp/src/main/java/com/app/springbootshoppingApp/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.app.springbootshoppingApp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.app.springbootshoppingApp.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200/")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long>{
 
