3aee60e1a5458f08d3c22b2bf831d52ea926cac3
kksrajput <kaushalkantsingh@gmail.com>
2022-11-11 17:35:11 +0530

    Sha Ang Routing

--- Angular-frontend/Angular-shopping/src/app/app.module.ts
+++ Angular-frontend/Angular-shopping/src/app/app.module.ts
@@ -5,13 +5,22 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import {  HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
+import  { Routes,RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component:  ProductListComponent },
+  { path: 'products', component:  ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch:  'full' },
+  { path: '**', redirectTo: '/products', pathMatch:  'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
