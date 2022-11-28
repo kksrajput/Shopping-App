dbbd2c547c945a40343a956fe741dcbabc2bb0c2
Kaushal Kant Singh <55576102+kksrajput@users.noreply.github.com>
2022-11-28 21:38:42 +0530

    Update SpringBootShoppingAppApplicationTests.java

--- spring-boot-shoppingApp/src/test/java/com/app/springbootshoppingApp/SpringBootShoppingAppApplicationTests.java
+++ spring-boot-shoppingApp/src/test/java/com/app/springbootshoppingApp/SpringBootShoppingAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootShoppingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
